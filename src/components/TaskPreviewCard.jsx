8a81bfbf7110f115c36f76f227efd21b39cc8740
noahemmenegger <noahemmenegger.business@gmail.com>
2022-09-17 12:51:34 +0200

    Add TaskPreviewCard

---
+++ src/components/TaskPreviewCard.jsx
@@ -0,0 +1,19 @@
+import { FontAwesome } from '@expo/vector-icons';
+import React from 'react';
+import { Text, View } from 'react-native';
+import tw from '../utils/tailwind';
+
+export default function TaskPreviewCard() {
+    return (
+        <View style={tw`bg-white m-5 p-5 rounded-lg shadow-md`}>
+            <View style={tw`flex flex-row`}>
+                <Text style={tw`mr-auto`}>TaskPreviewCard</Text>
+                <FontAwesome name="star-o" size={24} color="#FFD125" />
+            </View>
+            <View style={tw`flex flex-row w-full justify-between mt-3`}>
+                <Text style={tw`text-gray-400`}>Rotkreuz</Text>
+                <Text style={tw`text-gray-400`}>Noah Emmenegger</Text>
+            </View>
+        </View>
+    );
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,36 +1,59 @@
 import { FontAwesome } from '@expo/vector-icons';
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import TaskPreviewCard from '../components/TaskPreviewCard';
 import tw from '../utils/tailwind';
 
 export default function HomeScreen({ navigation }) {
     const [isMyList, setIsMyList] = useState(false);
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            title: 'Read me a bedtime story',
+            isRemote: true,
+            location: '',
+        },
+        {
+            id: 2,
+            title: 'Old woman wants to cross the road!',
+            isRemote: false,
+            location: 'Rotkreuz',
+        },
+    ]);
     return (
-        <View style={tw`bg-white rounded-xl`}>
-            <SafeAreaView style={tw`relative`}>
-                <View style={tw`flex flex-row justify-center`}>
-                    <Text
-                        style={tw`${!isMyList ? 'underline text-primary' : ''} m-2 mb-0 text-lg`}
-                        onPress={() => setIsMyList(false)}
-                    >
-                        Suggestion
-                    </Text>
-                    <Text
-                        style={tw`${isMyList ? 'underline text-primary' : ''} m-2 mb-0 text-lg`}
-                        onPress={() => setIsMyList(true)}
-                    >
-                        My list
-                    </Text>
-                </View>
+        <Fragment>
+            <View style={tw`bg-white rounded-xl`}>
+                <SafeAreaView style={tw`relative`}>
+                    <View style={tw`flex flex-row justify-center`}>
+                        <Text
+                            style={tw`${!isMyList ? 'underline text-primary' : ''} m-2 mb-0 text-lg`}
+                            onPress={() => setIsMyList(false)}
+                        >
+                            Suggestion
+                        </Text>
+                        <Text
+                            style={tw`${isMyList ? 'underline text-primary' : ''} m-2 mb-0 text-lg`}
+                            onPress={() => setIsMyList(true)}
+                        >
+                            My list
+                        </Text>
+                    </View>
 
-                <FontAwesome
-                    name="plus"
-                    size={30}
-                    style={tw`text-primary absolute top-15 right-10`}
-                    onPress={() => navigation.navigate('CreateProject')}
-                />
-            </SafeAreaView>
-        </View>
+                    <FontAwesome
+                        name="plus"
+                        size={30}
+                        style={tw`text-primary absolute top-15 right-6`}
+                        onPress={() => navigation.navigate('CreateProject')}
+                    />
+                </SafeAreaView>
+            </View>
+
+            <View style={tw`flex`}>
+                {tasks.map((task) => (
+                    <TaskPreviewCard key={task.id} task={task} />
+                ))}
+            </View>
+        </Fragment>
     );
 }
