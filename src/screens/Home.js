4aef6fadf49a2b31781160b22979e558b7007bc6
noahemmenegger <noahemmenegger.business@gmail.com>
2022-09-17 10:47:57 +0200

    Change design

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,6 @@
+import { FontAwesome } from '@expo/vector-icons';
 import React, { useState } from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import tw from '../utils/tailwind';
 
@@ -7,25 +8,27 @@ export default function HomeScreen({ navigation }) {
     const [isMyList, setIsMyList] = useState(false);
     return (
         <View style={tw`bg-white rounded-xl`}>
-            <SafeAreaView style={tw`flex flex-row justify-center`}>
-                <Text
-                    style={tw`${!isMyList ? 'underline text-primary' : ''} m-2 mb-0 text-lg`}
-                    onPress={() => setIsMyList(false)}
-                >
-                    Suggestion
-                </Text>
-                <Text
-                    style={tw`${isMyList ? 'underline text-primary' : ''} m-2 mb-0 text-lg`}
-                    onPress={() => setIsMyList(true)}
-                >
-                    My list
-                </Text>
+            <SafeAreaView style={tw`relative`}>
+                <View style={tw`flex flex-row justify-center`}>
+                    <Text
+                        style={tw`${!isMyList ? 'underline text-primary' : ''} m-2 mb-0 text-lg`}
+                        onPress={() => setIsMyList(false)}
+                    >
+                        Suggestion
+                    </Text>
+                    <Text
+                        style={tw`${isMyList ? 'underline text-primary' : ''} m-2 mb-0 text-lg`}
+                        onPress={() => setIsMyList(true)}
+                    >
+                        My list
+                    </Text>
+                </View>
 
-                <Button
-                    title="CreateProject"
-                    onPress={() =>
-                        navigation.navigate('CreateProject')
-                    }
+                <FontAwesome
+                    name="plus"
+                    size={30}
+                    style={tw`text-primary absolute top-15 right-10`}
+                    onPress={() => navigation.navigate('CreateProject')}
                 />
             </SafeAreaView>
         </View>
