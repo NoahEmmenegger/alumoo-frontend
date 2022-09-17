0f349629c50b30a6701ace007820b2df30dd4777
Kenny <kennybetschart03@gmail.com>
2022-09-17 07:44:40 +0200

    setup tailwind

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import tw from 'tailwind-react-native-classnames'
 
 export default function HomeScreen() {
     console.log('test');
     return (
         <SafeAreaView>
-            <Text>Home</Text>
+            <Text style={[tw`bg-red-600`]}>Home</Text>
         </SafeAreaView>
     );
 }
